4a4c9d38317d74979b7da1a222fc1001d543dded
yvanddniyo <nyvan80076@gmail.com>
2024-06-23 22:51:07 +0200

    adding new changes on the home page

---
+++ expression/add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    int y;
+    int z;
+
+    cout<< "Enter the value x and y";
+    cin>>x>>y;
+
+    z = x + y;
+    cout<<"\n"<<"Value of z is :"<<z;  
+  return 0;
+}
