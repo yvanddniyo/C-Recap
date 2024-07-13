a735833d337d60b86fb0288a78125126cb737318
yvanddniyo <nyvan80076@gmail.com>
2024-07-13 22:47:35 +0200

    adding new changes on the codes

--- conditionals/ifStatement.cpp
+++ conditionals/ifStatement.cpp
@@ -3,13 +3,6 @@
 using namespace std;
 
 int main () {
-    //   int num = 11;
-    //   if (num % 2 == 0){
-    //     cout << "it is even number" << endl;
-    //   }
-    //   else {
-    //     cout << "it is odd number" << endl;
-    //   }
     int num;
 
     cout << "Enter the numnber of your grade : " << endl;
