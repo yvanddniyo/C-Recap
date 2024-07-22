2cd0f3719d658adea027f84594053ee81883cd98
yvanddniyo <nyvan80076@gmail.com>
2024-07-22 22:19:32 +0200

    new changes in the in c++

--- conditionals/switch.cpp
+++ conditionals/switch.cpp
@@ -8,9 +8,9 @@ int main () {
     cout<<"Enter a number to check grade: ";
     cin>>num;
     switch(num) {
-        case 10: cout<<"it is 10" <<endl; break;
-        case 20: cout<<"it is 20" <<endl; break;
-        case 30: cout<<"it is 30" <<endl; break;
-        default:cout<<"Not 10, 20 or 30" <<endl; break; 
+        case 10: cout<<"it is 20" <<endl; break;
+        case 20: cout<<"it is 30" <<endl; break;
+        case 30: cout<<"it is 40" <<endl; break;
+        default:cout<<"Not 20, 30 or 40" <<endl; break; 
     }
 }
