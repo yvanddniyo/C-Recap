ad6ee9bbaa70ae893a9361613fc633efa49841c6
yvanddniyo <nyvan80076@gmail.com>
2025-09-16 00:03:39 +0200

    Learning about string

---
+++ course/variableAndType/introductionString/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+// Introdcution in string;
+using namespace std;
+int main() {
+  string mystring;
+   mystring = "Hello this is my initial string";
+   cout << mystring << endl;
+   mystring  = "Hello this is my second string";
+   cout << mystring << endl;
+  return 0;
+}
